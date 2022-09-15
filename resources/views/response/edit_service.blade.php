37c4a300b22c4323992c94884d506bb094cbaa75
inaam ul haq <waleedhaq339@gmail.com>
2022-09-15 16:09:35 +0500

    updated

--- resources/views/response/edit_service.blade.php
+++ resources/views/response/edit_service.blade.php
@@ -1,13 +1,13 @@
 <div class="row mb-3">
     <div class="col-md-12 col-12">
         <div class="card border-0 shadow-sm p-3">
-            <h5 class="card-title mb-2">Edit Category</h5>
-            <form action="{{ route('edit_category', $category->id) }}" method="get" class="row g-3 needs-validation" novalidate>
+            <h5 class="card-title mb-2">Edit service</h5>
+            <form action="{{ route('edit_service', $service->id) }}" method="get" class="row g-3 needs-validation" novalidate>
                 
                 <div class="col-md-12 mb-2">
-                    <label for="edit_category" class="form-label mb-2">Category: <span class="text-danger">*</span></label>
+                    <label for="edit_service" class="form-label mb-2">service: <span class="text-danger">*</span></label>
                     <div class="input-group has-validation">
-                        <input type="text" onfocus="get_map_category()" class="form-control" id="edit_category" name="name" value="{{ $category->name }}" placeholder="category"
+                        <input type="text" class="form-control" id="edit_service" name="name" value="{{ $service->name }}" placeholder="service"
                             required>
                         <div class="valid-feedback">
                             Looks good!
@@ -16,7 +16,7 @@
                             @if ($errors->has('name'))
                                 {{ $errors->first('name') }}
                             @else
-                                Please choose a category.
+                                Please choose a service.
                             @endif
                         </div>
                     </div>
